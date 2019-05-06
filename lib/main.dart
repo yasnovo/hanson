61149926587a99b7a1edfd09f89b605d80a760ad
yasnovo <yasnovo@gmail.com>
2019-05-06 10:41:30 +0900

    add todo item

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,102 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new TodoApp());
 
-class MyApp extends StatelessWidget {
+class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'TODO APP',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new TaskListPage(title: 'TODO Demo'),
+      home: new TodoList(title: 'TODO Demo'),
     );
   }
 }
 
-class TaskListPage extends StatefulWidget {
-  TaskListPage({Key key, this.title}) : super(key: key);
+class TodoList extends StatefulWidget {
+  TodoList({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _TaskListPageState createState() => new _TaskListPageState();
+  TodoListState createState() => new TodoListState();
 }
 
-class _TaskListPageState extends State<TaskListPage> {
-  List<String> _counter = new List<String>();
+class TodoListState extends State<TodoList> {
+  //List<String> _todoItems = new List<String>();
+  List<String> _todoItems = [];
 
-  void _incrementCounter() {
-    setState(() {
-       int cnt = _counter.length;
-      _counter.add('*' + cnt.toString());
-    });
+//  void _addTodoItems() {
+//    setState(() {
+//       int index = _todoItems.length;
+//      _todoItems.add('Item ' + index.toString());
+//    });
+//  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+          itemCount: _todoItems.length,
+          itemBuilder: (context, int index) {
+            if(index < _todoItems.length){
+              return _buildTodoItem(index);
+            }
+          },
+      );
+  }
+
+  Widget _buildTodoItem(int index) {
+    return ListTile(
+        title: Text('${_todoItems[index]}')
+    );
+
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(widget.title),
+        title: new Text(widget.title), // 持ち回さなくてもいい
       ),
-      body: new ListView.builder(
-          itemCount: _counter.length,
-          itemBuilder: (context, int index) {
-            return ListTile(
-                title: Text('${_counter[index]}')
-            );
-          },
-      ),
+      body: _buildTodoList(),
       floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _pushAddTodoScreen,
         tooltip: 'Increment',
         child: new Icon(Icons.add),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+
+  void _pushAddTodoScreen() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('add a new task'),
+            ),
+            body: TextField(
+              autofocus: true,
+              onSubmitted: (val) {
+                _addTodoItem(val);
+                Navigator.pop(context);
+              },
+              decoration: InputDecoration(
+                hintText: 'enter someting to do...',
+                contentPadding: const EdgeInsets.all(16),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void _addTodoItem(String task) {
+    if(task.length > 0){
+      setState(() => _todoItems.add(task));
+    }
   }
 }
